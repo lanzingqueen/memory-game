49c7fcad4e815f2b1222f859a07569bdca7a432e
Sarah Lanzing <sarahlanzing@gmail.com>
2018-07-09 21:16:06 -0400

    Review 1 JS

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 /*
- * Create a list that holds all of your cards
+ * Reset button
  */
 document.getElementsByClassName('restart')[0].addEventListener('click', reset);
+// Reset functionality (resets timer, moves, stars, match counter, shuffles deck, and flips all cards back over)
 function reset() {
     document.querySelectorAll('li.card').forEach(function(card) {
         card.classList.remove('open', 'show', 'match');
     });
     //shuffle(document.getElementsByClassName('card'));
     //console.log(document.getElementsByClassName('card'))
-    var deck = document.getElementsByClassName('card');
+    let deck = document.getElementsByClassName('card');
     deckArr = [];
-    for(var i = 0; i < deck.length; i++) {
+    for(let i = 0; i < deck.length; i++) {
         deckArr.push(deck[i]);
     }
     deckArr = shuffle(deckArr);
     //console.log(deckArr);
     deck.innerHTML = "";
     document.getElementById("deck").innerHTML = "";
-    for(var i = 0; i < deckArr.length; i++) {
+    for(let i = 0; i < deckArr.length; i++) {
         document.getElementById("deck").appendChild(deckArr[i]);
     }
-    var modal = document.getElementById('congrats');
+    let modal = document.getElementById('congrats');
     modal.style.display = "none";
     clearTimer();
     resetMoves();
@@ -28,15 +29,9 @@ function reset() {
     resetMatches();
 }
 
-
- /*   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -51,57 +46,65 @@ function shuffle(array) {
 }
 
 //figure out how to flip 2 cards and not the same card over and over
-var lastFlipped = null;
-var shouldClickable = true;
-var matchCount = 0;
+let lastFlipped = null;
+let shouldClickable = true;
+let matchCount = 0;
 
+//function for when each card is clicked
 function cardClicked(event) {
     card = event.target;
-    addMove();
      if (shouldClickable === false) {
-         console.log('cannot click again');
+         //console.log('cannot click again');
          return;
      }
+     //prevent clicking same card over and over
      if (lastFlipped === card) {
-         console.log('stop clicking yourself!');
+         //console.log('stop clicking yourself!');
          return;
      }
+     //prevent clicking when already matched
      if (card.classList.contains('match')) {
-         console.log("leave it alone");
+         //console.log("leave it alone");
          return;
      }
+     //start timer when a legit click has been made (if not already started)
      if (timer === undefined) {
          setTimer();
      }
+     //matched logic
      if(lastFlipped) {
          shouldClickable = false;
          card.classList.add('open', 'show');
-         console.log("second ", lastFlipped, card);
+         //console.log("second ", lastFlipped, card);
+         addMove();
          setTimeout(function() {
              if (lastFlipped.firstElementChild.classList[1] === card.firstElementChild.classList[1]) {
-                 console.log ("matched, yo!");
+                 //console.log ("matched, yo!");
                  lastFlipped.classList.remove('open', 'show');
                  lastFlipped.classList.add('match');
                  card.classList.remove('open', 'show');
                  card.classList.add('match');
                  matchCount++;
                  if (matchCount === 8) {
-                    console.log ('pop-up goes here');
+                    //console.log ('pop-up goes here');
                     gameOver();
                  };
                  //console.log(matchCount + " Match");
              }
              else {
+                 //flip back over and delay so user can see what the second card was
                  lastFlipped.classList.remove('open', 'show');
                  card.classList.remove('open', 'show');
-                 console.log("wrong");
+                 //console.log("wrong");
              }
+             //reset flipped and clickable logic for next move
              lastFlipped = null;
              shouldClickable = true;
          }, 300);
      }
-     //  compareCards (lastFlipped, card);
+     //  flip cards back over
      else {
+         addMove();
          lastFlipped = card;
          lastFlipped.classList.add('open', 'show');
          console.log("first clicked card ", card, card.firstElementChild, card.firstElementChild.classList[1])
@@ -113,22 +116,22 @@ function eachCard(card) {
 
 document.querySelectorAll('li.card').forEach(eachCard);
 
-//timer start and clear
-var time = 0;
-var timer = undefined;
+//timer start
+let time = 0;
+let timer = undefined;
 
 function setTimer() {
     timer = setInterval(function() {
         time++;
-        var str = "";
-        var minutes = Math.floor(time/60);
+        let str = "";
+        let minutes = Math.floor(time/60);
         if (minutes >= 0 && minutes <= 9) {
             str += "0" + minutes;
         } else {
             str += minutes;
         }
         str+=":";
-        var seconds = time % 60;
+        let seconds = time % 60;
         if(seconds >=0 && seconds <=9) {
             str += "0" + seconds;
         } else {
@@ -142,6 +145,7 @@ function setTimer() {
     }, 1000);
 };
 
+//timer clear
 function clearTimer() {
     clearInterval(timer);
     timer = undefined;
@@ -153,8 +157,8 @@ function clearTimer() {
 
 //when all cards have been matched
 function gameOver() {
-    var modal = document.getElementById('congrats');
-    var span = document.getElementsByClassName("close")[0];
+    let modal = document.getElementById('congrats');
+    let span = document.getElementsByClassName("close")[0];
     span.onclick = function() {
         modal.style.display = "none";
     }
@@ -169,7 +173,7 @@ function gameOver() {
 }
 
 //move counter
-var move = 0;
+let move = 0;
 starsRemoved = 0;
 function addMove() {
     move++;
@@ -177,22 +181,20 @@ function addMove() {
     movesText = document.querySelectorAll('.moves').forEach(function(moves){
         moves.innerHTML=Math.floor(move/2);
     });
-    //movesText.innerHTML = move;
     console.log('Move added');
-    //move this to somewhere else and make sure it doesn't get confused
     if (move == 20) {
-        var star = document.getElementById("stars").firstElementChild;
+        let star = document.getElementById("stars").firstElementChild;
         star.classList.add('hide-star');
         starsRemoved++;
         //console.log("star removed ,",star)
     } 
     if (move == 25) {
-        var star = document.getElementById("stars").children[1];
+        let star = document.getElementById("stars").children[1];
         star.classList.add('hide-star');
         //console.log("star removed ,",star)
         starsRemoved++;
     }
-    console.log('Stars Removed = ' + starsRemoved);
+    console.log(`Stars Removed = ${starsRemoved}`);
 }
 
 function pauseTimer() {
@@ -201,9 +203,9 @@ function pauseTimer() {
 }
 
 function modalStars() {
-    var starCount = '';
-    starCount = '' + (3-starsRemoved);
-    var star = document.querySelector('#modal-stars').innerHTML = starCount;// get star here;
+    let starCount = '';
+    starCount = `${3-starsRemoved}`;
+    let star = document.querySelector('#modal-stars').innerHTML = starCount;
 }
 
 function resetMatches() {
@@ -216,8 +218,8 @@ function resetMoves() {
 }
 
 function resetStars() {
-    var star1=document.getElementById('stars').children[0];
-    var star2=document.getElementById('stars').children[1];
+    let star1=document.getElementById('stars').children[0];
+    let star2=document.getElementById('stars').children[1];
     star1.classList.remove('hide-star');
     star2.classList.remove('hide-star');
     starsRemoved = 0;
